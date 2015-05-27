a0ebbba43bae7b8aa347308f467c4f7d97c2e13d
ymnk13 <zz.to13@gmail.com>
2015-05-27 19:13:37 +0900

    追加:ポイントの移動

--- mainTable.py
+++ mainTable.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# Last-Updated : <2015/05/27 15:50:48 by ymnk>
-from PyQt5.QtWidgets import (QApplication,QWidget,QMainWindow,QTableWidget,QTableWidgetItem,QLineEdit,QSlider,QLabel)
+# Last-Updated : <2015/05/27 19:12:34 by ymnk>
+from PyQt5.QtWidgets import (QApplication,QWidget,QMainWindow,QTableWidget,QTableWidgetItem,QLineEdit,QSlider,QLabel,QGraphicsWidget,QGraphicsScene,QGraphicsView,QGraphicsItem,QGraphicsEllipseItem)
 from PyQt5.QtWidgets import (QPushButton)
 from PyQt5.QtWidgets import (QHBoxLayout,QVBoxLayout)
 from PyQt5.QtCore import QSize
@@ -10,6 +10,27 @@ import sys,os
 import numpy as np
 import sqlite3
 
+class CircleItem(QGraphicsEllipseItem):
+    def __init__(self,x,y,width,height,pen,parent = None):
+        super(QGraphicsEllipseItem, self).__init__(x,y,width,height)
+        #self.setFlag(QGraphicsItem.ItemIsMovable)
+        self.setFlags(  self.flags()                            |
+                        QGraphicsItem.ItemIsSelectable          |
+                        QGraphicsItem.ItemIsMovable             |
+                        QGraphicsItem.ItemIsFocusable           |
+                        QGraphicsItem.ItemSendsScenePositionChanges )
+        
+    def paint(self,painter,option = None,widget = None):
+        painter.setBrush(Qt.red)
+        painter.drawEllipse(self.rect())
+    def itemChange(self, change, variant):
+        super(CircleItem, self).itemChange(change, variant)
+        if change == QGraphicsItem.ItemPositionChange:
+            #self.setParentItem(None)
+            None
+        return QGraphicsItem.itemChange(self, change, variant)
+
+ 
 class SlideController(QVBoxLayout):
     def __init__(self,maxFrame,parent = None):
         super(QVBoxLayout, self).__init__(parent)
@@ -55,6 +76,53 @@ class FrameController(QHBoxLayout):
 class MainWindow(QMainWindow):
     def __init__(self,parent = None):
         super(MainWindow, self).__init__(parent)
+        self.left = TableContents(self)
+        #self.right = TableContents()#
+        self.right = ImageContents(self)
+        self.mainLayout = QHBoxLayout()
+        
+        
+        self.mainLayout.addWidget(self.left)
+        self.mainLayout.addWidget(self.right)
+        self.setLayout(self.mainLayout)
+
+        mainWidget = QWidget(self)
+        mainWidget.setLayout(self.mainLayout)
+        self.setCentralWidget(mainWidget)
+        self.resize(1680,1050)
+
+class ImageContents(QWidget):
+    def __init__(self,parent = None):
+        super(QWidget, self).__init__(parent)
+        self.button0 = QPushButton("&<<")
+        self.button1 = QPushButton("&<<")
+        #self.graphics = QGraphicsWidget()
+        self.graphics = QGraphicsView()
+        #self.graphics.resize(1920,1028)
+
+        self.scene = QGraphicsScene(self)
+        blackpen = Qt.black
+        ellipse0 = self.scene.addEllipse(0,0,50,50,blackpen,blackpen)
+        ellipse1 = self.scene.addEllipse(1024,1028,100,100,blackpen,blackpen)
+        self.scene.addItem(CircleItem(60,60,60,60,Qt.black))
+        ellipse0.setFlag(QGraphicsItem.ItemIsMovable)
+        ellipse1.setFlag(QGraphicsItem.ItemIsMovable)
+        self.graphics.setScene(self.scene)
+        #self.graphics.scale(2000, 2000)
+        print self.scene.width()
+        self.topLayout = QVBoxLayout()
+        self.topLayout.addWidget(self.button0)
+        self.topLayout.addWidget(self.graphics)
+
+        self.mainLayout = QHBoxLayout(self)
+        self.mainLayout.addLayout(self.topLayout)
+        self.mainLayout.addWidget(self.button1)
+        self.setLayout(self.mainLayout)
+
+
+class TableContents(QWidget):
+    def __init__(self,parent = None):
+        super(QWidget, self).__init__(parent)
         self.conn = sqlite3.connect(os.path.abspath('./data/acceptData.db'))
         cur = self.conn.cursor()
         cur.execute("""SELECT DogTag,x,y FROM dates where frameN=1;""")
@@ -68,8 +136,8 @@ class MainWindow(QMainWindow):
         self.viewData(self.frameN)
 
 
-        self.mainWidget = QWidget(self)
-        self.mainLayout = QVBoxLayout(self.mainWidget)
+        #self.mainWidget = QWidget(self)
+        self.mainLayout = QVBoxLayout(self)
 
 
         # Button+Editor Version
@@ -91,10 +159,11 @@ class MainWindow(QMainWindow):
         #self.mainLayout.addLayout(self.LeftLayout)
         #self.mainLayout.addWidget(self.testButton)
 
-        self.mainWidget.setLayout(self.mainLayout)
-        self.setCentralWidget(self.mainWidget)
+        self.setLayout(self.mainLayout)
+        #self.setCentralWidget(self.mainWidget)
 
-        self.resize(1680,1050)
+        #
+
     def frameNEdited(self):
         text = self.frameEdit.displayText()
         if not text.isdigit():
