d726b9e7e8d841704299f2d8b2d607f0cb77ece4
Licht Takeuchi <licht-t@math.dis.titech.ac.jp>
2015-08-12 23:45:22 +0900

    Fixed filePath.py formats

--- filePath.py
+++ filePath.py
@@ -1,9 +1,10 @@
 import os.path
+import urllib
 
-currentDirPath = os.path.abspath(os.path.dirname(__file__) )
+currentDirPath = os.path.abspath(os.path.dirname(__file__))
 
 pythonLibDirPath = os.path.join(currentDirPath, 'lib', 'python')
-sampleDataPath = os.path.join(currentDirPath,"data")
-userDir        = os.path.expanduser('~')
+sampleDataPath = os.path.join(currentDirPath, "data")
+userDir = os.path.expanduser('~')
 
 blocklyURL = "file:///{0}".format(os.path.join(currentDirPath,"lib","editor","index.html").replace("\\","/"))
