46b9e56c9033c05e91fb3e300da6086632ef4863
Licht Takeuchi <licht-t@math.dis.titech.ac.jp>
2016-01-09 23:21:55 +0900

    Added the MainWindow title

--- main.py
+++ main.py
@@ -486,6 +486,7 @@ if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
     MainWindow = Ui_MainWindow()
     MainWindow.setWindowIcon(QIcon(':/icon/icon.ico'))
+    MainWindow.setWindowTitle('UMATracker-FilterGenerator')
     MainWindow.show()
     sys.exit(app.exec_())
 
